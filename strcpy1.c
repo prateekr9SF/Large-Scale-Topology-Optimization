8d6a09439bad721c324b1c531301d1de54b270be
Pranjan2 <119811887+pratran2390@users.noreply.github.com>
2024-04-15 23:20:04 -0400

    dgen for strcpy1.c

--- strcpy1.c
+++ strcpy1.c
@@ -1,19 +1,27 @@
-/*     CalculiX - A 3-dimensional finite element program                 */
-/*              Copyright (C) 1998-2018 Guido Dhondt                          */
-
-/*     This program is free software; you can redistribute it and/or     */
-/*     modify it under the terms of the GNU General Public License as    */
-/*     published by the Free Software Foundation(version 2);    */
-/*                                                                       */
-
-/*     This program is distributed in the hope that it will be useful,   */
-/*     but WITHOUT ANY WARRANTY; without even the implied warranty of    */ 
-/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      */
-/*     GNU General Public License for more details.                      */
-
-/*     You should have received a copy of the GNU General Public License */
-/*     along with this program; if not, write to the Free Software       */
-/*     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.         */
+/** 
+ * @file main.c
+ * @brief Implementation of custom string operations for CalculiX.
+ *
+ * CalculiX - A 3-dimensional finite element program.
+ * This file contains implementations of specific string operations 
+ * that are tailored to the needs of the CalculiX program, including
+ * a customized string copy function that pads with spaces if the source
+ * string is shorter than the desired length.
+ *
+ * Copyright (C) 1998-2018 Guido Dhondt.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation (version 2).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <stdlib.h>
 #include <math.h>
@@ -21,22 +29,33 @@
 #include <string.h>
 #include "main.h"
 
+/** 
+ * @brief Copies a string from source to destination with space padding.
+ *
+ * This function copies a string from 's2' to 's1' up to 'length' characters.
+ * If 's2' is shorter than 'length', the remainder of 's1' is filled with spaces.
+ * This is particularly useful in scenarios where strings need to be a fixed width.
+ *
+ * @param s1 Pointer to the destination string buffer.
+ * @param s2 Pointer to the source string to be copied.
+ * @param length The number of characters to copy or pad.
+ * @return Always returns 0.
+ */
 ITG strcpy1(char *s1, const char *s2, ITG length)
 {
-  ITG b,i,blank=0;
+    ITG b, i, blank = 0;
 
-  for(i=0;i<length;i++) {
-      if(blank==0){
-	  b=*s2;
-	  if(b=='\0')blank=1;
-      }
-      if(blank==0) {*s1=*s2;s2++;}
-      else *s1=' ';
-      s1++;
-  }
-  return 0;
+    for (i = 0; i < length; i++) {
+        if (blank == 0) {
+            b = *s2;
+            if (b == '\0') blank = 1;
+        }
+        if (blank == 0) { *s1 = *s2; s2++; }
+        else *s1 = ' ';
+        s1++;
+    }
+    return 0;
 }
-	  
 
 
 
