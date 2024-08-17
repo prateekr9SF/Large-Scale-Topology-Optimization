e0b65717734b8dadbcbb4aefbb6875e3be7ffbbe
Prateek Ranjan <prateekr@mit.edu>
2024-08-16 21:35:23 -0400

    writing disp

--- OLD/CALTOP_mkleqnnz_cluster/tecplot.f
+++ OLD/CALTOP_mkleqnnz_cluster/tecplot.f
@@ -35,12 +35,13 @@
       character*81 set(*),prset(*)
       character*87 filab(*)
 
+     
       integer nset,istartset(*),iendset(*),ialset(*),nprint,ipkon(*),
      &  mi(*),nstate_,ii,jj,iset,l,limit,node,ipos,ithermal,ielem,
      &  nelem,kon(*),inotr(2,*),ntrans,ielorien(mi(3),*),norien,nk,ne,
      &  inum(*),nfield,ikin,nodes,ne0,nope,mt,ielmat(mi(3),*),iface,
      &  jfaces,mortar,islavsurf(2,*),ielprop(*),nload,
-     &  nelemload(2,*)
+     &  nelemload(2,*), j
 
       real*8 v(0:mi(2),*),t1(*),fn(0:mi(2),*),stx(6,mi(1),*),bhetot,
      &  eei(6,mi(1),*),xstate(nstate_,mi(1),*),ener(mi(1),*),energytot,
@@ -57,13 +58,15 @@
 
       mt=mi(2)+1
 
+     
+
       !Open the .vtk file for writing
       open(unit=10, file='elastic_field.dat', status='unknown')
 
       !Write Tecplot 360 file header
       write(10,*) 'TITLE = "Simulation Results"'
-      write(10, '(A)') 'VARIABLES = ''X'', ''Y'', ''Z'',' //
-     &                '''DispX'', ''DispY'', ''DispZ'''
+      write(10, '(A)') 'VARIABLES = "X", "Y", "Z",' //
+     &                '"DispX", "DispY", "DispZ"'
 
      
 
@@ -89,12 +92,43 @@
 
       !     Write nodal coordinates
       do node = 1, nk
-        write(10, '(3F12.5)') co(1,node), co(2,node), co(3,node)
+        write(10, '(6F12.5)') co(1,node), co(2,node), co(3,node),
+     &        v(1,node), v(2,node), v(3,node)
       end do
 
 
-          
 
+      ! Write element connectivity based on the element type
+      ! Write element connectivity based on the element type
+      do ielem = 1, ne
+        select case (lakon(ielem)(1:4))
+        case ('C3D8')  ! 8-node hexahedral element
+           do j = 1, 8
+              write(10, '(I6)', advance='no') kon(ipkon(ielem) + j)
+           end do
+           write(10,*)  ! Newline after each element's nodes
+        case ('C3D4')  ! 4-node tetrahedral element
+           do j = 1, 4
+              write(10, '(I6)', advance='no') kon(ipkon(ielem) + j)
+           end do
+           write(10,*)  ! Newline after each element's nodes
+        case ('C3D6')  ! 6-node wedge element
+           do j = 1, 6
+              write(10, '(I6)', advance='no') kon(ipkon(ielem) + j)
+           end do
+           write(10,*)  ! Newline after each element's nodes
+        case ('C3D20')  ! 20-node quadratic hexahedral element
+           do j = 1, 20
+              write(10, '(I6)', advance='no') kon(ipkon(ielem) + j)
+           end do
+           write(10,*)  ! Newline after each element's nodes
+        ! Add more cases as needed for other element types
+        case default
+           ! Handle unknown or unsupported element types
+           write(10,*) 'Unknown element type: ', lakon(ielem)(1:4)
+        end select
+      end do
+          
 
       close(10)
 
